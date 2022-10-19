a5499cd8bc6adab1032639f05d2f0989b5be8251
willia13 <113810608+willia13@users.noreply.github.com>
2022-10-19 14:19:47 -0400

    Update ParentProcess.c
    combined ParentProcess.c and LikeServer.c

--- ParentProcess.c
+++ ParentProcess.c
@@ -2,3 +2,30 @@
 ParentProcess.c
 This file creates 10 child processes
 */
+void parentProcess(){
+  int times = 0;
+
+  cleanLogs();
+  makeLog("Parent Process began at: ", 0, 0){
+
+  
+  //creates 10 processes
+  for(int ii = 0; ii<9; ii++){
+    if (fork() == 0){
+      ii = 10; //ends loop
+    }
+    else
+      times++;      
+  }
+  makeLog("Child Process
+
+  if (times == 0){ // if the first parent
+    //wait();
+    
+  }
+  return;
+}
+
+int main(){
+  parentProcess();
+}
